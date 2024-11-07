4af5415a92dbeb6cb600bd25fffffa0a169fea83
Lê Minh Huy <78388860+huyhuyhuy8s@users.noreply.github.com>
2024-11-07 16:26:12 +0700

    npm init

---
+++ main.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import { engine } from 'express-handlebars'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+app.engine('hbs', engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+
+
+app.get('/', function (req, res) {
+    // res.send('Hello world');/
+    res.render('home', {
+        // layout: false,
+    });
+});
+
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.get('/test', function (req, res) {
+    res.sendFile(__dirname + '/test.html')
+});
+
+app.listen(3000, function (req, res) {
+    console.log('server started on http://localhost:3000');
+});
