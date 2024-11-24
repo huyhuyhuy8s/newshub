3c9f2c7f37d51824ace9d8e246e9aa949c07c6f1
huyhuyhuy8s <78388860+huyhuyhuy8s@users.noreply.github.com>
2024-11-24 21:05:29 +0700

    fix div name, element and add link into each element

--- ChuyenMuc/scriptMain.js
+++ ChuyenMuc/scriptMain.js
@@ -1,5 +1,5 @@
 let div1 = document.getElementsByClassName('div1')[0].offsetHeight;
-let div234 = document.getElementsByClassName('div-234')[0].offsetHeight;
+let div234 = document.getElementsByClassName('triple-highlight')[0].offsetHeight;
 let sum = div234 + div1;
 document.getElementsByClassName('child2')[0].style.height = `${sum}px`;
 
