20fb258419cb58c76886d3c6af2be074646a0413
huyhuyhuy8s <78388860+huyhuyhuy8s@users.noreply.github.com>
2024-11-28 07:10:48 +0700

    add search button

--- ChuyenMuc/scriptMain.js
+++ ChuyenMuc/scriptMain.js
@@ -1,11 +1,10 @@
-let div1 = document.getElementsByClassName('div1')[0].offsetHeight;
-let div234 = document.getElementsByClassName('triple-highlight')[0].offsetHeight;
-let sum = div234 + div1;
-document.getElementsByClassName('child2')[0].style.height = `${sum}px`;
+// set child2 height = child1 (include div1 and triple highlight)
+document.getElementsByClassName('child2')[0].style.height = `${document.getElementsByClassName('div1')[0].offsetHeight + document.getElementsByClassName('triple-highlight')[0].offsetHeight}px`;
 
 let navbar = document.getElementById("nav")
 let shouldStickPosition = navbar.offsetTop;
 
+// set sticky navbar
 function addOrRemoveStickyClass() {
     if (window.scrollY >= shouldStickPosition) {
         navbar.classList.add("sticky");
@@ -16,4 +15,30 @@ function addOrRemoveStickyClass() {
 
 window.onscroll = function () {
     addOrRemoveStickyClass();
+}
+
+let searchClickVar = false;
+// click and show on search icon
+function searchClick() {
+    if (searchClickVar) {
+        const li = document.getElementsByClassName("left-input")[0];
+        const ul = document.getElementsByClassName("left")[0];
+
+        console.log(1);
+
+        ul.removeChild(li);
+        searchClickVar = !searchClickVar;
+        return;
+    }
+    searchClickVar = !searchClickVar;
+    console.log(2);
+    const input = document.createElement("input");
+    input.type = "text";
+
+    const li = document.createElement("li");
+    li.appendChild(input);
+    li.className = "left-input";
+
+    const ul = document.getElementsByClassName("left")[0];
+    ul.appendChild(li);
 }
