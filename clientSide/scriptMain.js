d50559b008079e4c03ae609de86e592a7027ef37
huyhuyhuy8s <78388860+huyhuyhuy8s@users.noreply.github.com>
2024-12-05 12:13:32 +0700

    update login, register, otp

--- clientSide/scriptMain.js
+++ clientSide/scriptMain.js
@@ -1,4 +1,5 @@
 // set child2 height = child1 (include div1 and triple highlight)
+// Adjust the heights
 try {
     document.getElementsByClassName('child2')[0].style.height = `${document.getElementsByClassName('div1')[0].offsetHeight + document.getElementsByClassName('triple-highlight')[0].offsetHeight}px`;
 }
@@ -12,82 +13,135 @@ try {
     document.getElementsByClassName("article-big")[0].style.height = `${document.getElementsByClassName("article-small")[0].offsetHeight}px`;
 }
 catch { }
-let navbar = document.getElementById("nav")
-let shouldStickPosition = navbar.offsetTop;
 
-// set sticky navbar
-function addOrRemoveStickyClass() {
-    if (window.scrollY >= shouldStickPosition) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
+
+// Sticky navbar
+try {
+    let navbar = document.getElementById("nav")
+    let shouldStickPosition = navbar.offsetTop;
+
+    // set sticky navbar
+    function addOrRemoveStickyClass() {
+        if (window.scrollY >= shouldStickPosition) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+
+    window.onscroll = function () {
+        addOrRemoveStickyClass();
     }
 }
+catch { }
 
-window.onscroll = function () {
-    addOrRemoveStickyClass();
-}
 
-let searchClickVar = false;
-// click and show on search icon
-function searchClick() {
-    if (searchClickVar) {
-        const li = document.getElementsByClassName("left-input")[0];
-        const ul = document.getElementsByClassName("left")[0];
+// Search click icon
+try {
+    let searchClickVar = false;
+    // click and show on search icon
+    function searchClick() {
+        if (searchClickVar) {
+            const li = document.getElementsByClassName("left-input")[0];
+            const ul = document.getElementsByClassName("left")[0];
 
-        console.log(1);
+            console.log(1);
 
-        ul.removeChild(li);
+            ul.removeChild(li);
+            searchClickVar = !searchClickVar;
+            return;
+        }
         searchClickVar = !searchClickVar;
-        return;
+        console.log(2);
+        const input = document.createElement("input");
+        input.type = "text";
+
+        const li = document.createElement("li");
+        li.appendChild(input);
+        li.className = "left-input";
+
+        const ul = document.getElementsByClassName("left")[0];
+        ul.appendChild(li);
     }
-    searchClickVar = !searchClickVar;
-    console.log(2);
-    const input = document.createElement("input");
-    input.type = "text";
-
-    const li = document.createElement("li");
-    li.appendChild(input);
-    li.className = "left-input";
-
-    const ul = document.getElementsByClassName("left")[0];
-    ul.appendChild(li);
 }
+catch { }
 
-let nav_ul = document.getElementById('nav-ul');
-let subnav = document.getElementById('subnav');
-let targetnav;
+// subnav
+try {
+    let nav_ul = document.getElementById('nav-ul');
+    let subnav = document.getElementById('subnav');
+    let targetnav;
 
-nav_ul.addEventListener('mouseover', (e) => {
-    if (e.target.className !== '' && e.target.tagName !== 'a') {
-        targetnav = document.getElementById(e.target.className);
-        targetnav.hidden = false;
-        targetnav.addEventListener('mouseover', (e) => {
-            if (e.target.id !== '') {
-                targetnav.hidden = false;
+    nav_ul.addEventListener('mouseover', (e) => {
+        if (e.target.className !== '' && e.target.tagName !== 'a') {
+            targetnav = document.getElementById(e.target.className);
+            targetnav.hidden = false;
+            targetnav.addEventListener('mouseover', (e) => {
+                if (e.target.id !== '') {
+                    targetnav.hidden = false;
+                }
+            })
+
+            targetnav.addEventListener('mouseout', (e) => {
+                if (targetnav !== null) {
+                    targetnav.hidden = true;
+                }
+            })
+
+            for (const key in targetnav.children) {
+                var child_element = targetnav.children[key];
+                child_element.addEventListener('mouseover', (e) => {
+                    targetnav.hidden = false;
+                });
+                child_element.addEventListener('mouseout', (e) => {
+                    targetnav.hidden = true;
+                });
+            };
+        }
+    })
+
+    nav_ul.addEventListener('mouseout', (e) => {
+        if (targetnav !== null) {
+            targetnav.hidden = true;
+        }
+    })
+}
+catch { }
+
+
+// OTP
+try {
+    const inputs = document.getElementsByClassName("information")[0];
+
+    inputs.addEventListener("input", function (e) {
+        const target = e.target;
+        const val = target.value;
+
+        if (isNaN(val)) {
+            target.value = "";
+            return;
+        }
+
+        if (val != "") {
+            const next = target.nextElementSibling;
+            if (next) {
+                next.focus();
             }
-        })
+        }
+    });
 
-        targetnav.addEventListener('mouseout', (e) => {
-            if (targetnav !== null) {
-                targetnav.hidden = true;
+    inputs.addEventListener("keyup", function (e) {
+        const target = e.target;
+        const key = e.key.toLowerCase();
+
+        if (key == "backspace" || key == "delete") {
+            target.value = "";
+            const prev = target.previousElementSibling;
+            if (prev) {
+                prev.focus();
             }
-        })
-
-        for (const key in targetnav.children) {
-            var child_element = targetnav.children[key];
-            child_element.addEventListener('mouseover', (e) => {
-                targetnav.hidden = false;
-            });
-            child_element.addEventListener('mouseout', (e) => {
-                targetnav.hidden = true;
-            });
-        };
-    }
-})
-
-nav_ul.addEventListener('mouseout', (e) => {
-    if (targetnav !== null) {
-        targetnav.hidden = true;
-    }
-})
+            return;
+        }
+    });
+}
+catch { }
