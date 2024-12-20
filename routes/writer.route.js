2e6bc3346c902a33b20ab59cd8404c14bb67a024
huyhuyhuy8s <78388860+huyhuyhuy8s@users.noreply.github.com>
2024-12-20 11:37:11 +0700

    update chart in editor and writer home

--- routes/writer.route.js
+++ routes/writer.route.js
@@ -17,17 +17,16 @@ router.get('/list-post', async (req, res) => {
 });
 
 router.get('/create-article', async (req, res) => {
-    // let news = {
-    //     Content: tinymce.activeEditor.getContent("article"),
-    //     Title: document.getElementById('title').value,
-    //     Premium: document.getElementById('premium').checked,
-    //     Category: document.getElementById('category').value,
-    //     Meta_title: document.getElementById('meta-title').value,
-    //     Meta_description: document.getElementById('meta-description').value,
-    // }
-    const a = await writerService.findWriter(id_user)
-    console.log(a);
-    // link: document.getElementById('link').value,
+    let news = {
+        Id_Writer: await writerService.findWriter(id_user),
+        Id_Status: "STS0001",
+        Content: tinymce.activeEditor.getContent("article"),
+        Title: document.getElementById('title').value,
+        Premium: document.getElementById('premium').checked,
+        Category: document.getElementById('category').value,
+        Meta_title: document.getElementById('meta-title').value,
+        Meta_description: document.getElementById('meta-description').value,
+    }
 
 
     res.render('vwWriter/create_article', { layout: 'moderator' });
